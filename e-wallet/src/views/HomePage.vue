4f92a3c861f2623b79691b648ca109609396d8cf
GungdeAngga <agungangga2001@gmail.com>
2022-09-26 18:59:42 +0800

    main content done and need to link the router

--- e-wallet/src/views/HomePage.vue
+++ e-wallet/src/views/HomePage.vue
@@ -1,20 +1,21 @@
 <template>
   <div class="HomePage">
     <HeaderWeb />
- 
+    <MainContateWeb />
     
   </div>
 </template>
 
 <script>
 import HeaderWeb from "@/components/HeaderWeb.vue";
+import MainContateWeb from "../components/MainContateWeb.vue";
 
 
 export default {
   name: "HomePage",
   components: {
     HeaderWeb,
-
+    MainContateWeb
 },
 };
 
