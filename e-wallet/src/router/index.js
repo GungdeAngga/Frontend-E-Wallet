c259afc849bfa4dd1b7c7b317cb5a84e7420860b
GungdeAngga <agungangga2001@gmail.com>
2022-09-26 09:06:08 +0800

    sort file for work

---
+++ e-wallet/src/router/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import HomeView from '../views/HomePage.vue'
+
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'HomePage',
+    component: HomeView
+  },
+  
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
---
+++ e-wallet/src/views/HomePage.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="HomePage">
+    
+  </div>
+</template>
+
+<style>
+
+</style>
+
+<script>
+
+</script>
