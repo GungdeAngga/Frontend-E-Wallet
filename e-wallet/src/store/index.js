768126991821eebdce1eec0acf632f41724adb0b
GungdeAngga <agungangga2001@gmail.com>
2022-09-29 14:56:37 +0800

    adding vuex

---
+++ e-wallet/src/store/index.js
@@ -0,0 +1,15 @@
+import Vue from "vue";
+import Vuex from "vuex";
+import createPersistedState from "vuex-persistedstate";
+
+Vue.use(Vuex);
+const persistedDataState = createPersistedState({
+  paths: [" , "],
+});
+
+export default new Vuex.Store({
+  plugins: [persistedDataState],
+  modules: {
+
+  },
+});
